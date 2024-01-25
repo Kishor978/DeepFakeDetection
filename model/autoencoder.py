ffe98bd1af417eb400eb9d149b9b15fea8872136
Kishor978 <thaunnakishor@gmail.com>
2024-01-25 17:00:21 +0545

    encoder and decoder class

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -30,4 +30,26 @@ class Encoder(nn.Module):
             nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), padding=0)
             )
             
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.features=nn.Sequential(
+            nn.ConvTranspose2d(256,128,kernel_size=(2,2), stride=(2,2)),
+            nn.ReLU(inplace=True),
+            
+            nn.ConvTranspose2d(128, 64,kernel_size=(2,2),stride=(2,2)),
+            nn.ReLU(inplace=True),
+            
+            nn.ConvTranspose2d(64,32, kernel_size=(2,2),stride=(2,2)),
+            nn.ReLU(inplace=True),
+            
+            nn.ConvTranspose2d(32,16,kernel_size=(2,2),stride=(2,2)),
+            nn.ReLU(inplace=True),
+            
+            nn.ConvTranspose2d(16,3,kernel_size=(2,2),stride=(2,2)),
+            nn.ReLU(inplace=True)
+
+        )
+        
         
