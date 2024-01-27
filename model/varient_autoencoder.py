4d983acfaae7c32d61f6a009cc68074b1d4dcac6
Kishor978 <thaunnakishor@gmail.com>
2024-01-28 00:03:00 +0545

    Decoder in a variational autoencoder (VAE)

--- model/varient_autoencoder.py
+++ model/varient_autoencoder.py
@@ -3,15 +3,15 @@ from torch import nn
 from torchvision import transforms 
 from timm import create_model 
 from config import load_config
-
+  
 class Encoder(nn.Module):
     """Encoder in a variational autoencoder (VAE). 
     It is responsible for transforming input images into a lower-dimensional representation. 
     latent space is characterized by mean (mu) and variance (var) parameters, 
     which are used to sample from a Gaussian distribution to generate latent vectors
     """
-    def __init__(self,latent_dims=4) -> None:
-        super().__init__()
+    def __init__(self,latent_dims=4):
+        super(Encoder,self).__init__()
         
         self.features=nn.Sequential(
             nn.Conv2d(3,16,kernel_size=3,stride=2,padding=1),
@@ -61,3 +61,33 @@ class Encoder(nn.Module):
         # encourage the learned latent space to be close to a standard Gaussian distribution.
         self.kl = self.kl_weight*torch.mean(-0.5*torch.sum(1+var - mu**2 - var.exp(), dim=1), dim=0) 
         return z
+    
+    
+class Decoder(nn.Module):
+    """Decoder in a variational autoencoder (VAE). 
+    Is responsible for transforming the latent vector back into an image by using transpose convolutional layers"""
+    def __int__(self,latent_dims=4):
+        super(Decoder,self).__init__()
+        
+        self.features = nn.Sequential(
+            nn.ConvTranspose2d(256, 64, kernel_size=2, stride=2),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(16, 3, kernel_size=2, stride=2),
+            nn.LeakyReLU()
+        )
+
+        self.latent_dims = latent_dims
+        
+        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(256, 7, 7))
+
+    def forward(self, x): 
+        x = self.unflatten(x)
+        x = self.features(x)
+        return x
