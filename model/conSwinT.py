e6758cd7470404989a26b4f9a6ced76f8e5f37ef
Kishor978 <thaunnakishor@gmail.com>
2024-02-01 23:26:01 +0545

    hybrid model of Swin transformer, auto encoder and Varient autoencoder completed

--- model/conSwinT.py
+++ model/conSwinT.py
@@ -35,3 +35,26 @@ class ConSwinT(nn.Module):
                 raise Exception(f"Error: weight/{vae}.pth file not found.")
         else:
             try:
+                self.model_ed = ConvAutoEncoder(config)
+                self.model_vae = ConvVAE(config)
+                self.checkpoint_ed = torch.load(f'weight/{ed}.pth', map_location=torch.device('cpu'))
+                self.checkpoint_vae = torch.load(f'weight/{vae}.pth', map_location=torch.device('cpu'))
+                self.model_ed.load_state_dict(self.checkpoint_ed)
+                self.model_vae.load_state_dict(self.checkpoint_vae)
+                self.model_ed.eval()
+                self.model_vae.eval()
+                if self.fp16:
+                    self.model_ed.half()
+                    self.model_vae.half()
+            except FileNotFoundError as e:
+                raise Exception(f"Error: Model weights file not found.")
+    def forward(self, x):
+        if self.net == 'ed' :
+            x = self.model_ed(x)
+        elif self.net == 'vae':
+            x,_ = self.model_vae(x)
+        else:
+            x1 = self.model_ed(x)
+            x2,_ = self.model_vae(x)
+            x =  torch.cat((x1, x2), dim=0) #(x1+x2)/2 #
+        return x
