296ad3cf73b25f856ec06f7b821f48779c335de9
Kishor978 <thaunnakishor@gmail.com>
2024-02-23 09:34:19 +0545

    some utils functions needed for function

--- model/prediction_utils.py
+++ model/prediction_utils.py
@@ -7,6 +7,8 @@ import dlib
 from torchvision import transforms
 import tqdm
 from decord import VideoReader,cpu
+
+from dataloader import normalize_data
 from .config import load_config
 from .conSwinT import ConSwinT
 
@@ -64,4 +66,41 @@ def face_recog(frames):
             else:
                 break
     return ([],0) if count==0 else (temp_face[:count],count)
-     
+
+
+def preprocess_frame(frame):
+    """function is designed to preprocess a single frame for input into a neural network model.
+        Return:
+            a preprocessed tensor suitable for input into a neural network model.
+    """
+    df_tensor=torch.tensor(frame,device=device).float()
+    df_tensor=df_tensor.permute((0,3,1,2))
+    
+    for i in range(len(df_tensor)):
+        df_tensor[i]=normalize_data()["vid"](df_tensor[i]/255.0)
+        
+    return df_tensor
+
+def pred_video(df,model):
+    """ function is used to perform inference on a video represented as a sequence of frames.
+        Inputs:
+            df:video data in the form of tensor where each frame is stacked along the first dimension.
+            model: This is the neural network model used for prediction.
+        Output:
+            The function returns the maximum prediction value obtained from the model's output.
+    """
+    with torch.no_grad():
+        return max_prediction_value(torch.sigmoid(model(df).squeeze()))
+    
+def max_prediction_value(y_pred):
+    """Finds the index and value of the maximum prediction value"""
+    mean_val=torch.mean(y_pred,dim=0)
+    return(
+        torch.argmax(mean_val).item(),
+        mean_val[0].item()
+        if mean_val[0]>mean_val[1]
+        else abs(1-mean_val[1]).item()
+    )
+    
+def real_or_fake(prediction):
+    return {0:"Real",1:"Fake"}[prediction^1]
