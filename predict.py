732170aeca2424e42457412ef7adf8954bd6cb6e
Kishor978 <thaunnakishor@gmail.com>
2024-02-28 14:25:25 +0545

    Prediction function completed

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import os
 import json
+import argparse
 from time import perf_counter
+
 from datetime import datetime
 from model.prediction_utils import *
 
@@ -78,3 +80,83 @@ def celeb(root_dir="Celeb-DF-v2", dataset=None, num_frames=15, net=None, fp16=Fa
             print(f"An error occurred x: {str(e)}")
 
     return result
+
+
+def predict(
+    vid,
+    model,
+    fp16,
+    result,
+    num_frames,
+    net,
+    klass,
+    count=0,
+    accuracy=-1,
+    correct_label="unknown",
+    compression=None,
+):
+    count += 1
+    print(f"\n\n{str(count)} Loading... {vid}")
+
+    df = df_face(vid, num_frames, net)  # extract face from the frames
+    if fp16:
+        df.half()
+    y, y_val = (
+        pred_video(df, model)
+        if len(df) >= 1
+        else (torch.tensor(0).item(), torch.tensor(0.5).item())
+    )
+    result = store_result(
+        result, os.path.basename(vid), y, y_val, klass, correct_label, compression
+    )
+
+    if accuracy > -1:
+        if correct_label == real_or_fake(y):
+            accuracy += 1
+        print(
+            f"\nPrediction: {y_val} {real_or_fake(y)} \t\t {accuracy}/{count} {accuracy/count}"
+        )
+
+
+def gen_parser():
+    parser = argparse.ArgumentParser("ConSwinT prediction")
+    parser.add_argument("--p", type=str, help="video or image path")
+    parser.add_argument(
+        "--f", type=int, help="number of frames to process for prediction"
+    )
+    parser.add_argument(
+        "--d", type=str, help="dataset type, dfdc, faceforensics, timit, celeb"
+    )
+    parser.add_argument("--n", type=str, help="network ed or vae")
+    parser.add_argument("--fp16", type=str, help="half precision support")
+
+    args = parser.parse_args()
+    path = args.p
+    num_frames = args.f if args.f else 15
+    dataset = args.d if args.d else "other"
+    net = args.n if args.n in ["ed", "vae"] else "genconvit"
+    fp16 = True if args.fp16 else False
+
+    return path, dataset, num_frames, net, fp16
+
+
+def main():
+    start_time = perf_counter()
+    path, dataset, num_frames, net, fp16 = gen_parser()
+    result = (
+        globals()[dataset](path, dataset, num_frames, net, fp16)
+        if dataset in ["dfdc", "faceforensics", "timit", "celeb"]
+        else vids(path, dataset, num_frames, net, fp16)
+    )
+
+    curr_time = datetime.now().strftime("%B_%d_%Y_%H_%M_%S")
+    file_path = os.path.join("result", f"prediction_{dataset}_{net}_{curr_time}.json")
+
+    with open(file_path, "w") as f:
+        json.dump(result, f)
+    end_time = perf_counter()
+    print("\n\n--- %s seconds ---" % (end_time - start_time))
+
+
+if __name__ == "__main__":
+    main()
